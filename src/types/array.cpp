3a1856726dce73d7953d3a4d8340d0ca082baaa2
hyrdbyrd <IRuss23@yandex.ru>
2019-03-15 23:42:51 +0300

    feature: upd array

--- src/types/array.cpp
+++ src/types/array.cpp
@@ -2,40 +2,50 @@
 
 using namespace std;
 
-namespace Pomaransh
-{
+namespace Pomaransh {
     template<typename T>
-    class Array
-    {
+    class Array {
         public:
-            int length()
-            {
+            int length() {
                 return this->len;
             }
 
-            int toVector()
-            {
+            vector<T> to_vector() {
                 return this->list;
             }
 
-            Array()
-            {}
+            Array() {}
 
             template<typename ...REST>
-            Array(const REST &...rest)
-            {
-                this->push(rest...);
-            }
+            Array(const REST &...rest) { this->push(rest...); }
 
-            int push(const T content)
-            {
+            int push(const T content) {
                 this->list.push_back(content);
                 return ++this->len;
             }
 
+            int pop() {
+                this->list.pop_back();
+                return --this->len;
+            }
+
+            T get(const int idx) {
+                return this->list[idx];
+            }
+
+            template<typename C>
+            Array<C> map(T (*handler)(C, int, Array<T>&)) {
+                Array<C> newArray;
+
+                for (unsigned idx = 0; idx < this->length; idx++) {
+                    newArray.push(handler(this->list, idx, &this));
+                }
+
+                return newArray;
+            }
+
             template<typename ...REST>
-            int push(const T first, const REST &...items)
-            {
+            int push(const T first, const REST &...items) {
                 this->push(first);
                 this->push(items...);
             }
--- src/types/array.test.cpp
+++ src/types/array.test.cpp
@@ -1,15 +1,41 @@
-#include "array.cpp"
 #include <iostream>
+#include <assert.h>
+#include "array.cpp"
 
 using namespace std;
 using namespace Pomaransh;
 
-int main() {
+void testContructor() {
+    Array<int> a(1, 2, 3);
+
+    assert(a.length() == 3);
+}
+
+void testPop() {
+    // Should add two items
     Array<int> a;
+    a.push(1, 2);
+    assert(a.length() == 2);
 
-    a.push(3, 2, 3);
+    // Should be correct
+    Array<int> b;
+    const int item = 1;
+    b.push(item);
+    assert(b.get(0) == item);
+}
 
-    cout << a.length();
+void testPush() {
+
+}
+
+void testMap() {
+
+}
+
+int main() {
+    testContructor();
+    testPush();
+    testPop();
 
     return 0;
 }
